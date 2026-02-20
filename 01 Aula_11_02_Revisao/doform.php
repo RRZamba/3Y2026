d9cd5cd1c2678a9199028e8ff08459327e5a1c54
RRZamba <114082625+RRZamba@users.noreply.github.com>
2026-02-20 08:47:08 -0300

    Add files via upload

--- 01 Aula_11_02_Revisao/doform.php
+++ 01 Aula_11_02_Revisao/doform.php
@@ -1,4 +1,8 @@
 <?php
+    
+    //0) Conexao do DB
+	include('conexao.php');
+
 	//1) Recebendo os valores do form
 	$nome = $_POST['nome'];
 	$sobrenome = $_POST['sobrenome'];
@@ -11,9 +15,28 @@
 	//Caminho onde será salvo o arquivo
 	$caminho = "C:/wamp64/www/Projeto_11_02_Revisao/imagens/$arqNome";
 
+	//Variável para montar a query de insert
+	$query = "INSERT INTO migo(
+		 nome,sobrenome,curso,foto)
+	     VALUES('$nome','$sobrenome',
+	     	  '$curso','$caminho')";
+
+	//Executando o sql
+	$result = mysqli_query($con,$query);
+
+	//Verificando insert
+	if(!$result)
+	{
+		echo "<h3>Erro ao inserir no DB</h3>";
+		echo "<h4>Falar com o ADM!!!</h4>";
+		echo "<h4> $query </h4>"	;	
+		die();
+	}
+
+
 	//Salvando a #$#¨@$@#% do arquito!!! :/
 	if(@move_uploaded_file($arqTemp,$caminho)) 
-	{
+	{	
 		//Arquivo salvo
 		echo "<h3>Upload com sucesso, bebê!</h3>";
 		echo "<br>";
@@ -26,7 +49,9 @@
 		echo "Entrar em contato com o suporte";
 		echo "<h3>SEU OTÁRIO!!!</h3>";
 	}
-
-
-
 ?>
+
+
+
+  
+  A partir do projeto desenvolvido em sala de aula, construa uma nova página em php que faça um select de todas as informações do banco , incluindo a imagem e mostre na tela
