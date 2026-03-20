2cb2095a562257b0ab167a0e0f4e5c4b53850c6e
RRZamba <114082625+RRZamba@users.noreply.github.com>
2026-03-20 07:32:38 -0300

    Add files via upload

---
+++ 03 Aula_20_03_SistemaMilGrau/Modelo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Listagem</title>
+	<meta charset="utf-8">
+    
+    <!-- Link do CSS/Bootstrap-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- Link do JS/Bootstrap-->
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Lista Pessoas</h1>
+			<p>Pessoas Lindas!!!</p>
+		</div>
+	</div>
+</body>
+</html>
